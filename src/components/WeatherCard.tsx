c7082e1110ab36da2738e588b28d1cbb4310e4c5
Moshira89 <mushiraaljak@example.com>
2025-05-30 10:37:42 +0200

    fix something

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -10,7 +10,10 @@ type Props = {
 export const WeatherCard: React.FC<Props> = ({ city, temp, description, icon }) => (
   <div className="weather-card">
     <h2>{city}</h2>
-    <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt={description} />
+    <img
+      src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+      alt={description}
+    />
     <p>{description}</p>
     <p>{temp} °C</p>
   </div>
